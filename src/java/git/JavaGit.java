c99e6263bc144b75d32aaf059bf0540e7feb8ae7
Vojtěch Vojtěch <2017-e-vojtech@AD.GYARAB.CZ>
2020-09-07 11:19:45 +0200

    bagr

--- src/java/git/JavaGit.java
+++ src/java/git/JavaGit.java
@@ -15,7 +15,9 @@ public class JavaGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("ahoj");
+        System.out.println("ahoj bagr");
+        System.out.println("ahoj bagr");
+        System.out.println("ahoj bagr");
     }
     
 }
